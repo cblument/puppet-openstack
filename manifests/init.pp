1a55f053cb065c1438a14942edc33ed1eacda9a3
Chris <cblument@gmail.com>
2011-05-19 10:25:22 -0700

    Removing require for apt

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,7 @@ class openstack( $novaConfHash ) {
 
   package { $prePackages: ensure => installed }
 
-  package { "python-greenlet":
-    ensure => present,
-    require => Apt::Sources_list["nova_core_repo"]
-  }
+  package { "python-greenlet": ensure => present }
 
   package { ["nova-common", "nova-doc"]:
     ensure => present,
